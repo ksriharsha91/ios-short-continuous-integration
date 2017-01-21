dd31c7755d1f3001637040a387238957c7785d75
ksriharsha91 <ksriharsha@live.com>
2017-01-21 09:09:33 +0200

    set unit and ui tests to always run

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -34,6 +34,16 @@ class RomanNumeralCalculatorTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            
+/*Now that you’ve seen a unit test, you may have questions about the testPerformanceExample function that appears by default when creating a new unit test bundle for an app. This is a placeholder function for a performance test which we won’t be showing for the RNC app since it isn’t doing anything that requires a long-running process. In general, a performance test would be used to verify that some process can execute within an expected amount of time.
+ 
+ If you’d like to learn more about performance testing (and unit testing, in general), take a look at these Apple resources:
+ 
+ Apple: Testing Basics
+ WWDC 2016: Advanced Testing and Continuous Integration
+ WWDC 2014: Continuous Integration with Xcode 6 (a bit dated, but still useful)
+ WWDC 2014: Testing in Xcode 6 (also a bit dated, but still useful)
+ Test Driven Development by Kent Beck*/
         }
     }
     
